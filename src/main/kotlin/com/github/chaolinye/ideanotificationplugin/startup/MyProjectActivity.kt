bf0478bbb0009337606cfe5c0b01ff73e7103008
GitHub Action <action@github.com>
2025-10-22 02:57:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/chaolinye/ideanotificationplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/chaolinye/ideanotificationplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.chaolinye.ideanotificationplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
